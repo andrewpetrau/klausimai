7798b3b78dba40d567ea9acdcb23a856133bfcbb
Andrius Petrauskas <petrauskasandrius9@gmail.com>
2023-09-09 19:27:18 +0300

    pataisiau prisijungima kuriu pagrindini puslapi

--- klausimai/forms.py
+++ klausimai/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
+from .models import Klausimas
 class RegistracijosForma(UserCreationForm):
     pass
--- klausimai/views.py
+++ klausimai/views.py
@@ -11,3 +11,18 @@ def registracija(request):
     else:
         forma = RegistracijosForma()
     return render(request, 'registration/register.html', {'forma': forma})
+
+
+def pagrindinis(request):
+    if request.method == 'POST':
+        tekstas = request.POST.get('tekstas')
+        Klausimas.objects.create(tekstas=tekstas, uzduotas_vartotojas=request.user)
+        return redirect('pagrindinis')
+
+    neatsakyti = Klausimas.objects.filter(atsakymas__isnull=True)
+    atsakyti = Klausimas.objects.exclude(atsakymas__isnull=True)
+
+    return render(request, 'pagrindinis.html', {
+        'neatsakyti': neatsakyti,
+        'atsakyti': atsakyti
+    })
