2dc219d79c8b38faa2fa8f2a3d0b7bc9df1434af
Andrius Petrauskas <petrauskasandrius9@gmail.com>
2023-09-09 17:05:47 +0300

    registracija

--- klausimai/views.py
+++ klausimai/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .forms import RegistracijosForma
 # Create your views here.
+
+def registracija(request):
+    if request.method == 'POST':
+        forma = RegistracijosForma(request.POST)
+        if forma.is_valid():
+            forma.save()
+            return redirect('pagrindinis')
+    else:
+        forma = RegistracijosForma()
+    return render(request, 'registration/register.html', {'forma': forma})
