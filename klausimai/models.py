8002372f4df7933b20dd6d048571abf80cc3a50e
Andrius Petrauskas <petrauskasandrius9@gmail.com>
2023-09-09 15:25:13 +0300

    sukuriau modeli

--- klausimai/models.py
+++ klausimai/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Klausimas(models.Model):
+    tekstas = models.TextField()
+    uzduotas_vartotojas = models.ForeignKey(User, on_delete=models.CASCADE, related_name="klausimai")
+    atsakymas = models.TextField(blank=True, null=True)
+    atsake_vartotojas = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name="atsakymai")
